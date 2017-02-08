3326f737c21897e3073b2129ec9acbf7d18cf24d
Leif Walsh <leif.walsh@gmail.com>
2017-02-07 21:18:30 -0500

    Update conf.py
    mock dependencies when building docs

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -21,6 +21,17 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return Mock()
+
+# Mock these to avoid import errors when running doc generation on readthedocs.
+MOCK_MODULES = ['py4j', 'pyspark', 'pyspark.sql', 'pyspark.sql.types']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
